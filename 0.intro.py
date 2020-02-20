88674671ad7fcc2375eb3840f675d81076230235
Zou <zoushiqi040@gmail.com>
2020-02-20 19:35:04 +0800

    first test

--- 0.intro.py
+++ 0.intro.py
@@ -3,9 +3,11 @@
 '''
 @author: Steven Zou
 @contact: Shiqi_zou@163.com
+@address: HIT Harbin.China
 @software: Pycharm
+@version: Python2.7
 @file: 0.intro.py
-@time: 2020/2/2017:52
-@desc:
+@time: 2020/2/2018:22
 '''
+
 print('hello,算法图解')
