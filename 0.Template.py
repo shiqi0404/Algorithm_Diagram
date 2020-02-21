e1c7d649ef03293f69c9e4380475beba4597de80
邹世琦 <zoushiqi0404@gmail.com>
2020-02-21 14:16:54 +0800

    Initial Test

---
+++ 0.Template.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+-------------------------------------------------
+Project Name:   Algorithm Diagram 
+File Name：     0.Template
+Description :   
+Author :        Steven.zou
+E-mail:         zoushiqi0404@gmail.com
+Date：          2020/2/21 14:15
+Software:       PyCharm
+-------------------------------------------------
+ Change Activity:
+                   2020/2/21:
+-------------------------------------------------
+"""
+print ('This is a Template.')
+print ('这是一个模板')
