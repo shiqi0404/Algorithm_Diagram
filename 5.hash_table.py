c81eeb18af4f95aec0aa74af05c41bd0710994e1
邹世琦 <zoushiqi0404@gmail.com>
2020-03-31 12:04:52 +0800

    Update

---
+++ 5.hash_table.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+-------------------------------------------------
+Project Name:   Algorithm Diagram 
+File Name：     5.hash_table
+Description :   散列表，最有用的基本数据结构之一。
+Author :        Steven.zou
+E-mail:         zoushiqi0404@gmail.com
+Date：          2020-03-31 9:45
+Software:       PyCharm
+-------------------------------------------------
+ Change Activity:
+                   2020-03-31:
+-------------------------------------------------
+"""
+
+book = dict()           # 散列表，在python中为字典，直接调用dict()
+
+book["apple"] = 0.67    # 一个苹果的价格为67美分
+book["milk"] = 1.49     # 牛奶的价格为1.49美元
+book["avocado"] = 1.49
+
+print book
+print book["avocado"]   # 鳄梨的价格
